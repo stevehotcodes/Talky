e13262609b56be54a42f5a17bff20c6d3c48280e
Stephen Ondieki <stevehotcodes@gmail.com>
2023-12-11 21:05:35 +0300

    added the validators

--- frontend-talky/src/app/components/login/login.component.ts
+++ frontend-talky/src/app/components/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+
+  loginForm!:FormGroup
+
+  constructor (private formBuilder:FormBuilder){}
+
+
+  ngOnInit(){
+    this.loginForm=this.formBuilder.group({
+      email:['',[Validators.required,Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}')]],
+      password:['',[Validators.required,Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}')]]
+    })
+
+  }
 
 }
--- frontend-talky/src/app/components/sign-up/sign-up.component.ts
+++ frontend-talky/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Validators,FormGroup, FormBuilder} from '@angular/forms'
 
 @Component({
   selector: 'app-sign-up',
@@ -6,9 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+  signupForm!:FormGroup
+
+  constructor(private formBuiilder:FormBuilder){}
 
   ngOnInit(){
+    this.signupForm=this.formBuiilder.group({
+      fullName:['',[Validators.required]],
+      userName:['',[Validators.required]],
+      email:['',[Validators.required,Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}')]],
+      password:['',[Validators.required,Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}')]]
+    })
     
+  }
+
+  submit(){
+    console.log(this.signupForm.value)
   }
 
 }
